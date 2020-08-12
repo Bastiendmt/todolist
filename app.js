d97fa760ff5e5412586569bd6872a7d085fe847d
bastienxs <bastienxs@gmail.com>
2020-08-12 16:39:31 +0200

    tp

--- app.js
+++ app.js
@@ -7,22 +7,39 @@ var app = express();
 
 
 /* On utilise les sessions */
-app.use(session({secret: 'todotopsecret'}))
+app.use(session({ secret: 'todotopsecret' }))
 
+    /* S'il n'y a pas de todolist dans la session,
+    on en crée une vide sous forme d'array avant la suite */
+    .use(function (req, res, next) {
+        if (typeof (req.session.todolist) == 'undefined') {
+            req.session.todolist = [];
+        }
+        next();
+    })
 
-/* Gestion des routes en-dessous
-   ....                         */
-var app = express();
+    /* Gestion des routes en-dessous
+       ....                         */
+    .get('/todo', function (req, res) {
+        res.render('todo.ejs', { todolist: req.session.todolist })
+    })
 
-app.get('/todo', function (req, res) {
-
-})
-
-    .post('/todo/add', function (req, res) {
-
+    .post('/todo/add', urlencodedParser, function (req, res) {
+        if (req.body.newtodo != '') {
+            req.session.todolist.push(req.body.newtodo);
+        }
+        res.redirect('/todo');
     })
 
     .get('/todo/delete/:id', function (req, res) {
+        if (req.params.id != '') {
+            req.session.todolist.splice(req.params.id, 1);
+        }
+        res.redirect('/todo');
+    })
 
+    .use(function (req, res, next) {
+        res.redirect('/todo');
     });
 
+app.listen(8080);
